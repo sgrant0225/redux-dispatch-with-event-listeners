e699eec338cbab60b296c1fdd529c6c32f81beff
github username <sharnellgrant@gmail.com>
2021-12-01 23:11:15 -0500

    commit tree eb5001d11634b2a7f7bda396493958350ff211a9

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,32 @@
 // add code snippets from README
+let state;
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+
+
+//we can reassign the state by adding the dispatch function
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }
+
+  //By calling this function is does return our default state and then call render.
+  dispatch({type: '@@INIT'})
+
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
